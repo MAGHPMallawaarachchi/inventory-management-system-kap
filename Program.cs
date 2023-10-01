5873519b8edfc8189afa0aace7cb3e8e2afacb45
Hasini Punsala <hasinipunsala@gmail.com>
2023-10-01 21:31:58 +0530

    chore: initialize file structure

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace inventory_management_system_kap
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Dashboard());
         }
     }
 }
