71c7402f7c8d9ac6fe3df4f5025bb9a20813c1d0
Dilantha-Wijesinghe <wdilantha66@gmail.com>
2023-10-09 00:06:33 +0530

    chore: add labels and text boxes

--- Views/NewCustomerModalView.Designer.cs
+++ Views/NewCustomerModalView.Designer.cs
@@ -28,12 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewCustomerModalView));
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2Panel3 = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2Panel4 = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2Panel5 = new Guna.UI2.WinForms.Guna2Panel();
+            this.imgBtnClose = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.lblHeading = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2Panel1.SuspendLayout();
+            this.guna2Panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // guna2Panel1
@@ -52,6 +56,8 @@
             // 
             // guna2Panel2
             // 
+            this.guna2Panel2.Controls.Add(this.imgBtnClose);
+            this.guna2Panel2.Controls.Add(this.lblHeading);
             this.guna2Panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.guna2Panel2.Location = new System.Drawing.Point(15, 15);
             this.guna2Panel2.Name = "guna2Panel2";
@@ -86,6 +92,29 @@
             this.guna2Panel5.Size = new System.Drawing.Size(254, 484);
             this.guna2Panel5.TabIndex = 3;
             // 
+            // imgBtnClose
+            // 
+            this.imgBtnClose.CheckedState.Parent = this.imgBtnClose;
+            this.imgBtnClose.HoverState.Parent = this.imgBtnClose;
+            this.imgBtnClose.Image = ((System.Drawing.Image)(resources.GetObject("imgBtnClose.Image")));
+            this.imgBtnClose.ImageSize = new System.Drawing.Size(15, 15);
+            this.imgBtnClose.Location = new System.Drawing.Point(349, 7);
+            this.imgBtnClose.Name = "imgBtnClose";
+            this.imgBtnClose.PressedState.Parent = this.imgBtnClose;
+            this.imgBtnClose.Size = new System.Drawing.Size(30, 23);
+            this.imgBtnClose.TabIndex = 3;
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.BackColor = System.Drawing.Color.Transparent;
+            this.lblHeading.Font = new System.Drawing.Font("Inter Medium", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(230)))), ((int)(((byte)(235)))));
+            this.lblHeading.Location = new System.Drawing.Point(11, 5);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(111, 31);
+            this.lblHeading.TabIndex = 2;
+            this.lblHeading.Text = "New Item";
+            // 
             // NewCustomerModalView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -96,6 +125,8 @@
             this.Name = "NewCustomerModalView";
             this.Text = "NewCustomerModalView";
             this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel2.ResumeLayout(false);
+            this.guna2Panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -107,5 +138,7 @@
         private Guna.UI2.WinForms.Guna2Panel guna2Panel3;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel4;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel5;
+        private Guna.UI2.WinForms.Guna2ImageButton imgBtnClose;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblHeading;
     }
 }
